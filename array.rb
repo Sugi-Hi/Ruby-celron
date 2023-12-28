d99f42678b1aefb1b310c64b209a5a148391676d
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-12-28 21:57:15 +0900

    配列への追加・代入

--- array.rb
+++ array.rb
@@ -6,3 +6,13 @@ pencil_case << "付箋"
 
 puts pencil_case
 puts pencil_case.length
+
+
+#----------------------------------------------------
+# TechCamp演習問題
+# 問題1 
+animals = ["イヌ", "キリン", "ライオン"]
+# 問題2
+animals << "パンダ"
+# 問題2
+animals[0] = "ネコ"
