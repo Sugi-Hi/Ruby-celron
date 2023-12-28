cc5e9de8f4df8d519e1e281f52e724e85ab49b0b
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-12-28 21:58:37 +0900

    繰返し文の回数目・合計値

--- loop.rb
+++ loop.rb
@@ -2,3 +2,18 @@ colors = ["あか", "あお", "きいろ"]
 colors.each do |color|
   puts "色: #{color}"
 end
+
+
+#----------------------------------------------------
+# TechCamp演習問題
+# 問題1 
+10.times do |i|
+  puts "#{i+1}回目の繰り返し"
+end
+# 問題2 
+sum = 0
+10.times do |i|
+  sum += (i+1)
+end
+
+puts "最後に全て足した値#{sum}"
