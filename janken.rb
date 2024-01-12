e85c8dd5c739ff39dedef60878d15c9436bac858
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-01-13 08:13:13 +0900

    ジャンケンアプリ想定

--- janken.rb
+++ janken.rb
@@ -1,3 +1,40 @@
 def janken
+  
+  play = ["グー", "チョキ", "パー"]
+
+  while true do
+  puts "グー:0, チョキ:1, パー:2⇒数字(0~2)で入力して下さい！"
+  you_player = gets.to_i
+  pc_player = rand(3)
+
+    if you_player >= 0 && you_player <= 2
+    break
+    end
+    
+  end
+
+  puts "あなたの手:#{play[you_player]},相手コンピュータの手:#{play[pc_player]}"
+
+  if you_player == pc_player
+    puts "あいこでショ！" 
+    return true
+  elsif (you_player==0 && pc_player==1) || (you_player==1 && pc_player==2) || (you_player==2 && pc_player==0)
+    puts "あなたの勝ちです！" 
+    return false 
+  else
+    puts "あなたの負けです！" 
+    return false 
+
+  end
 
 end
+
+puts "最初はグー、ジャイケンポイ！"
+
+next_play = true
+while next_play do
+  next_play = janken
+end
+
+
+
