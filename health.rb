60927650bd52f835b090b1daf52270c2d21bd2bc
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-01-26 18:35:22 +0900

    健康診断の受診結果

--- health.rb
+++ health.rb
@@ -28,6 +28,12 @@ def index_health(healths)
 end
 
 def show_health(health)
+  puts "氏名：#{health[:name]}"
+  puts "年齢：#{health[:age]}"
+  puts "歩行時間：#{health[:walktime]}"
+  puts "移動距離：#{health[:distance]}"
+  puts "身長：#{health[:height]}"
+  puts "体重：#{health[:weight]}"
 
 end
 
