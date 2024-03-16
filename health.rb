33da92e28bb0d53cab67c11515ac72cb035167f9
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-03-16 19:27:23 +0900

    健康診断の番号入力

--- health.rb
+++ health.rb
@@ -49,5 +49,10 @@ puts "[1]健康診断の全受診者を一覧リストで表示する"
 puts "[2]健康状態の目安となる数値データで表記する"
 puts "[3]現在のストレスチェックで健康レベルを計測する"
 puts "[4]終了する"
+input = gets.to_i
 
-
+case input
+when 0
+    def register_health(healths)
+when 1
+    def index_health(healths)
